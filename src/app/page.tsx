83900e478b5fe8b8ada46b71b37c6f8c03cc8be8
faun3 <antonioculcea@gmail.com>
2023-08-23 16:27:42 +0300

    fix homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,41 @@
 import Link from "next/link";
+import ImageCard from "@/components/ImageCard";
+import il1 from "@/public/il1.jpg";
+import il2 from "@/public/il2.jpg";
+import il3 from "@/public/il3.jpg";
 
 export default function Home() {
   return (
-    <main className="absolute w-full">
-      <div className="bg-slate-900 h-[5rem]"></div>
-      <div className="w-[95%] mx-auto bg-white rounded-xl relative -top-[5rem] px-10 py-6">
-        <h1 className="text-3xl font-bold text-center mb-2 mt-2">
-          Inverator is an app that allows you to easily manage your invoices.
-        </h1>
-        <h2 className="mb-10 text-center text-gray-600">
-          Get ready for hassle free invoice management. <br />
-          With Inverator, tracking your invoices to help with budgeting is so
-          easy, it almost feels fun.
-        </h2>
-        <div>
-          <h3 className="text-xl font-bold">
-            With Inverator&apos; easy to use interface you can:
-          </h3>
-          <div className="grid grid-cols-2 gap-6">
-            <div className="border-2 rounded-lg border-slate-900">
-              view all of your invoices
+    <main className="block h-fit">
+      <div className="w-full overflow-hidden">
+        <div className="bg-slate-900 h-[5rem]"></div>
+        <div className="w-[95%] mx-auto bg-white rounded-xl relative -top-[5rem] px-10 py-10">
+          <h1 className="text-3xl font-bold text-center mb-2 mt-2">
+            Inverator is an app that allows you to easily manage your invoices.
+          </h1>
+          <h2 className="mb-10 text-center text-gray-600">
+            Get ready for hassle free invoice management. <br />
+            With Inverator, tracking your invoices to help with budgeting is so
+            easy, it almost feels fun.
+          </h2>
+          <div>
+            <h3 className="text-xl font-bold">
+              With Inverator&apos; easy to use interface you can:
+            </h3>
+            <div className="grid grid-cols-2 gap-6 items-center justify-center place-content-center">
+              <ImageCard src={il1}>view all of your invoices</ImageCard>
+              <ImageCard src={il2}>
+                get a detailed view of a single invoice
+              </ImageCard>
+              <ImageCard src={il3}>edit and delete an invoice</ImageCard>
+              <div className="relative h-full flex flex-col justify-center items-center">
+                <Link
+                  href={"/invoices"}
+                  className="bg-slate-900 rounded-lg text-xl font-bold text-white text-center hover:bg-blue-700 transition-all absolute z-10 w-[75%] h-[75%] inline-flex items-center justify-center">
+                  Try Inverator
+                </Link>
+              </div>
             </div>
-            <div>get a detailed view of a single invoice</div>
-            <div>edit and delete an invoice</div>
-          </div>
-          <div className="mx-auto w-fit my-10">
-            <Link
-              href={"/invoices"}
-              className="bg-slate-900 px-8 py-6 rounded-full text-xl font-bold text-white inline-block text-center mx-auto  hover:bg-blue-700 transition-all">
-              Try Inverator
-            </Link>
           </div>
         </div>
       </div>
