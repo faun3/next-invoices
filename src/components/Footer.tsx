6b91b5114fc87b23eed5964de40c67f9cc0aa4dc
faun3 <antonioculcea@gmail.com>
2023-08-23 16:27:17 +0300

    make footer visible hopefully

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="h-[5rem] bg-slate-900 text-center text-gray-400 flex flex-col justify-center">
+    <footer className="h-[5rem] bg-slate-900 text-center text-gray-400 flex flex-col justify-center z-10">
       <a href="https://www.freepik.com/free-vector/invoice-concept-illustration_8845366.htm#query=invoice%20illustration&position=0&from_view=keyword&track=ais">
         Images by storyset
       </a>{" "}
