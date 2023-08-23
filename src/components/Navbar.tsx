fbccf4e86f3b53af5dcab795e299389d02b50378
faun3 <antonioculcea@gmail.com>
2023-08-23 14:28:33 +0300

    style nav active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,37 @@
+"use client";
+
 import { GiCrestedHelmet } from "react-icons/gi";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const Navbar = () => {
+  const currentRoute = usePathname();
   return (
-    <nav className="bg-gray-900 h-[8rem] sticky top-0 py-6 px-6">
+    <nav className="bg-gray-900 h-[6rem] sticky top-0 px-20 flex flex-row items-center justify-between">
       <div className="flex flex-row gap-2 items-center">
         <GiCrestedHelmet className="text-white h-[4rem] w-[4rem]" />
         <span className="text-white font-bold tracking-widest text-xl">
           INVERATOR
         </span>
       </div>
+      <ul className="text-white flex flex-row gap-4">
+        <li>
+          <Link
+            href={"/"}
+            className={`${currentRoute === "/" ? "underline font-bold" : ""}`}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href={"/invoices"}
+            className={`${
+              currentRoute === "/invoices" ? "underline font-bold" : ""
+            }`}>
+            Invoices
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 };
