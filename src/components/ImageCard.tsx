505bae73fabc45f47419f8adf987929b75fa61a0
faun3 <antonioculcea@gmail.com>
2023-08-23 16:27:38 +0300

    write imagecard component

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
-const ImageCard = ({ src }: { src: string }) => {
+const ImageCard = ({
+  src,
+  children,
+}: {
+  src: StaticImageData;
+  children: React.ReactNode;
+}) => {
   return (
     <div>
-      <Image src={src} alt=""></Image>
+      <Image src={src} alt="" width={600} height={600}></Image>
+      <div className="text-center">{children}</div>
     </div>
   );
 };
