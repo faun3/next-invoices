4ebf3df3056d6cf04d186e3bd91abca0be5fc70d
faun3 <antonioculcea@gmail.com>
2023-08-23 13:33:56 +0300

    define item interface

---
+++ src/types/Item.ts
@@ -0,0 +1,10 @@
+export default interface IItem {
+  rawName: string;
+  genericName: string;
+  category: number;
+  quantity: number;
+  quantityUnit: string;
+  productCode: string;
+  price: number;
+  totalPrice: number;
+}
