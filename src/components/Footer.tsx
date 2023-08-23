3fa0a00baaa03ab7ac4b39ad16c378641e04490c
faun3 <antonioculcea@gmail.com>
2023-08-23 15:46:29 +0300

    style footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="h-[5rem] bg-slate-900 text-center text-gray-400 flex flex-col justify-center">
+      <a href="https://www.freepik.com/free-vector/invoice-concept-illustration_8845366.htm#query=invoice%20illustration&position=0&from_view=keyword&track=ais">
+        Images by storyset
+      </a>{" "}
+      on Freepik
+    </footer>
+  );
+};
+export default Footer;
